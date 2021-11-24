740984b9e2c9f0c3775e3c0a4a966915fa9fd622
Jonas Svåsand <jonassvasand@gmail.com>
2021-11-24 22:18:15 +0100

    Fixed css issue, seemed to be some format issue when unzipping css file, fixed it with a python script formatting the css file

---
+++ Fixing css issues/fix_css.py
@@ -0,0 +1,18 @@
+
+lower = 'abcdefghijklmnopqrstuvwxyz'
+upper = lower.upper()
+other = ' .:;,?=)([]}{\\/*-_\'"@' #did not include @ in run, fixxed manually
+numbers = '1234567890'
+allchars = lower+upper+other+numbers
+
+old =  open("old_css.txt", "r")
+new = open('new_css.txt', 'w')
+
+lines = old.readlines()
+
+for line in lines:
+    for i in line:
+        if i in allchars:
+            new.write(i)
+    new.write('\n')
+
