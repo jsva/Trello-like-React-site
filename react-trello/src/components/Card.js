19272e6a40c0328f2e36165729c79a17c45ae9c3
Jonas Svåsand <jonas.svasand@student.uib.no>
2021-11-24 17:15:04 +0100

    Added different container classes following the steps in part 2 of the tutorial

---
+++ react-trello/src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Card extends React.Component {
+
+
+    render() {
+        return (
+            <p>card component</p>
+        );
+    }
+}
+
+export default Card;
