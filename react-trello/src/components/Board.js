41fd8f961ebc593449539840831922cda391484a
Jonas Svåsand <jonassvasand@gmail.com>
2021-11-25 14:56:21 +0100

    4-2 Added method for creating new lists with title entered by user.

--- react-trello/src/components/Board.js
+++ react-trello/src/components/Board.js
@@ -12,24 +12,48 @@ class Board extends React.Component {
         this.setState( {currentLists: data.lists});
     }
 
-    createNewList = () => {
+    addBoardInput = React.createRef()
+
+    createNewList = (e) => {
+        e.preventDefault();
         const list = {
             id: Math.random(),
-            title: "My amazing list",
+            title: this.addBoardInput.current.value,
             board: 300,
-            createdAt: new Date()
+            createdAt: new Date(),
+            cards: [
+                {
+                    id: 1,
+                    text: 'Card 1'
+                },
+                {
+                    id: 2,
+                    text: 'Card 2'
+                }
+            ]
         }
-        this.setState( {currentLists: [...this.state.currentLists, list]});
+        if(list.title) {
+            this.setState( {currentLists: [...this.state.currentLists, list]});
+            this.addBoardInput.current.value = '';
+        }
     }
 
     render() {
         return (
+            <div>
             <div className="'lists-wrapper">
-                <button onClick= {this.createNewList}>New List</button>
                 {Object.keys(this.state.currentLists).map(key => 
                     <List key={this.state.currentLists[key].id}
                     list={this.state.currentLists[key]} />
                 )}
+            </div>
+            <form onSubmit={this.createNewList} className="new-list-wrapper">
+                <input 
+                type="text"
+                ref = {this.addBoardInput}
+                name="name"
+                placeholder=" + New List" />
+            </form>
             </div>
         );
     }
