617feeb17ba79b10041b32f3889163b2042a9951
Jonas Svåsand <jonassvasand@gmail.com>
2021-11-25 17:28:15 +0100

    5-1 Added routing to app. Note that this project is using an old version of react-router-dom and will break with a newer version

--- react-trello/src/App.js
+++ react-trello/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Board from './components/Board';
 import data from './sampleData';
 import Home from './components/pages/Home';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import PageNotFound from './components/pages/PageNotFound';
 
 
 class App extends React.Component {
@@ -21,12 +23,32 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Home boards={this.state.boards} 
-        createNewBoard= {this.createNewBoard}/>
-        <Board />
+        <BrowserRouter>
+            <Route exact path="/" component={PageNotFound}/>
+            <Route path="/board" component={Board} />
+            <Route component={PageNotFound} />
+          {/* { <Home boards={this.state.boards} 
+            createNewBoard= {this.createNewBoard}/>
+            <Board />} */}
+        </BrowserRouter>
       </div>
   );
 }
 }
 
 export default App;
+
+
+// Code for routing if using updated version of react-router-dom 
+// this is for pre ver 6 of it. 
+
+// import { BrowserRouter, Route, Routes, Switch } from 'react-router-dom';
+// <BrowserRouter>
+// <Routes>
+//   <Route path="/board" element={<Board />} />
+//   <Route path='' element={<PageNotFound />} />
+// {/* { <Home boards={this.state.boards} 
+//   createNewBoard= {this.createNewBoard}/>
+//   <Board />} */}
+// </Routes>
+// </BrowserRouter>
