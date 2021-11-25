3f30f8c6f8c331bc9fbf6d709eaa4eecc7ebc799
Jonas Svåsand <jonassvasand@gmail.com>
2021-11-25 14:44:28 +0100

    4-1 Created form that can take in data from user, and also added functionality so the data can be used to create a new board

--- react-trello/src/components/pages/Home.js
+++ react-trello/src/components/pages/Home.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import BoardPreview from '../BoardPreview';
 import PropTypes from 'prop-types';
-
+import CreateBoardForm from '../CreateBoardForm';
 
 class Home extends React.Component {
-    
-    newBoard = () => {
-        const board = {
-            title: 'Trips to take this year',
-            background: '#80ffaa',
-            createdAt: new Date()
-        }
-        this.props.createNewBoard(board);
-    }
 
     render() {
         return (
             <div>
-                <button onClick={this.newBoard}>New board!</button>
+            <CreateBoardForm createNewBoard={this.props.createNewBoard} />
+            <div className="board-preview-wrapper">
             {
                 Object.keys(this.props.boards).map(key => (
                 <BoardPreview 
@@ -27,6 +19,7 @@ class Home extends React.Component {
             ))
         }
         </div>
+        </div>
         );
     }
 }
@@ -35,5 +28,7 @@ Home.propTypes = {
     boards: PropTypes.array.isRequired,
     createNewBoard: PropTypes.func.isRequired
 }
+
+
 
 export default Home;
